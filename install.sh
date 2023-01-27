15e6f751c2075cdfcaa5f13ab51f091ead6b598a
Tahir <mightnottaken@gmail.com>
2023-01-27 15:26:33 +0530

    adding file at desktop

--- install.sh
+++ install.sh
@@ -38,11 +38,13 @@ sudo cp -r ./rfidauth.service /etc/systemd/system/rfidauth.service;
 echo "created rfidauth.service";
 sudo chmod +x /usr/local/bin/rfidauth.sh;
 
+sudo cp -r ./frontend.sh ~/Desktop/frontend.sh;
 sudo cp -r ./frontend.sh /usr/local/bin/frontend.sh;
 echo "copied frontend.sh file in /usr/local/bin";
 sudo cp -r ./frontend.desktop /usr/share/applications/frontend.desktop;
 echo "created frontend.service";
 sudo chmod +x /usr/local/bin/frontend.sh;
+sudo chmod +x ~/Desktop/frontend.sh;
 
 sudo systemctl daemon-reload;
 echo "systemctl daemon reloaded";
@@ -64,7 +66,6 @@ echo "rfidexit service enabled";
 sudo systemctl stop rfidexit.service;
 sudo systemctl start rfidexit.service;
 echo "rfidexit service started";
-
 
 
 sudo systemctl enable rfidauth.service;
